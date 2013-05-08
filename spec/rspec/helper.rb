81e46b148af52c6900bbea33e046d98bf1e418fd
Jordon Bedwell <envygeeks@gmail.com>
2013-05-08 16:18:32 -0500

    Add Coveralls

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -1,9 +1,34 @@
 $:.unshift(File.expand_path("../../lib", __FILE__))
 ENV["RACK_ENV"] = "test"
 
-if ENV["SIMPLECOV"] == true
+unless %W(no false).include?(ENV["COVERAGE"])
   require "simplecov"
-  SimpleCov.start
+  require "coveralls"
+
+  module Coveralls
+    NoiseBlacklist = [
+      "[Coveralls] Using SimpleCov's default settings.".green,
+      "[Coveralls] Set up the SimpleCov formatter.".green,
+      "[Coveralls] Outside the Travis environment, not sending data.".yellow
+    ]
+
+    def puts(message)
+      # Only prevent the useless noise on our terminals, not inside of the Travis or Circle CI.
+      unless NoiseBlacklist.include?(message) || ENV["TRAVIS"] == "true" || ENV["CI"] == "true"
+        $stdout.puts message
+      end
+    end
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+        Coveralls::SimpleCov::Formatter
+  ]
+
+  Coveralls.noisy = true
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
 end
 
-%w(rack rack-csrf rspec rspec/mocks/standalone).each { |file| require file }
+%w(rack rack-csrf rspec).each { |file| require file }
